d3d2d6289896e88254fd6a7178907668da4dd239
LenaP <piskova.olenka@gmail.com>
2016-11-27 10:45:51 +0200

    final

--- rozrajs/diafragma.js
+++ rozrajs/diafragma.js
@@ -2,26 +2,27 @@
         Calculate diafragma by distance and focus
 
 **/
-var diafragma = function(focus, r_max, r_centr, delta) {
-        var f2 = Math.pow(focus, 2);
-        var ch = r_max / (r_centr * f2) - f2;
-        return ch/(delta*(r_centr - focus));
+var diafragma = function(focus, r_center, r_grip, delta) {
+        var r_max =  r_center + r_grip;
+        var f2 = Math.pow(focus*1000, 2);
+        var ch = r_max / (r_center * f2) + f2;
+        return ch/(delta*(r_center - focus))/1000/1000;
 };
 
 var calc_and_apply_to_DOM = function() {
 
     var formd = document.getElementById("deep");
 
-    var focus = formd.elements["focus"].value;
-    var delta = formd.elements["delta"].value;
-    var r_min = formd.elements["r_min"].value;
-    var r_center = formd.elements["r_center"].value;
-    var r_max = formd.elements["r_max"].value;
+    var focus = +formd.elements["focus"].value;
+    var delta = +formd.elements["delta"].value;
+    var r_center = +formd.elements["r_center"].value;
+    var r_grip = +formd.elements["r_grip"].value;
 
-    var result = diafragma(focus, r_max, r_center, delta);
+    document.getElementById("r1").innerHTML = r_center - r_grip;
+    document.getElementById("r2").innerHTML = r_center + r_grip;
 
-    var dia = document.getElementById("diafragma_value");
-    dia.innerHTML = (''+result).substr(0,6);
+    var result = diafragma(focus, r_center, r_grip, delta);
+    document.getElementById("diafragma_value").innerHTML = (''+result).substr(0,6);
 }
 
 var update = function(el, caption_id) {
