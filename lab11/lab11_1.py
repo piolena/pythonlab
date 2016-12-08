5660a6c063430dc8077dc97a549d86259f379f2a
LenaP <piskova.olenka@gmail.com>
2016-12-08 23:34:45 +0200

    lab11 ready

---
+++ lab11/lab11_1.py
@@ -0,0 +1,40 @@
+from save import *
+from Sequence import *
+
+seq = Sequence()
+print('start sequense is: ', seq.as_str())
+
+result = {}
+
+action = input('Input a action:')
+
+while action != 'stop':
+    if action == 'show all':
+        print(result)
+    elif action[:5] == "show ":
+        try:
+            key = action[5:]
+            print(key, ':', result[key])
+            break
+        except KeyError:
+            print('No such key in result dictionary!, try another please')
+    elif action == 'save':
+        save(result)
+    elif action == 'load':
+        result = load()
+    elif action == 'add':
+        seq.add_random()
+        result[action] = seq.as_str()
+    elif action == 'delete':
+        seq.delete_first()
+        result[action] = seq.as_str()
+    elif action == 'mutation':
+        seq.mutation()
+        result[action] = seq.as_str()
+    elif action == 'rnd_mutation':
+        seq.rnd_mutation()
+        result[action] = seq.as_str()
+    else:
+        result[action] = 'no data'
+    # get next
+    action = input('Input a action:')
